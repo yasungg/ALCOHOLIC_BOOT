770022ee84e40ba30578ca4bde65087ac8e5a91f
yasungg <dragxn1026@gmail.com>
2023-09-08 01:11:03 +0900

    Spring Security filterChain 코드 작성 중

--- src/main/java/ALCOHOLIC_BOOT/server/config/WebSecurityConfig.java
+++ src/main/java/ALCOHOLIC_BOOT/server/config/WebSecurityConfig.java
@@ -5,10 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +25,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
